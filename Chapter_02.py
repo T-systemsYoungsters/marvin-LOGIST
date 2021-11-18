d1ebb574894fccb572b81b767f1e666a57ab0745
MarvinKhn <marvinkh13@gmail.com>
2021-11-18 20:42:18 +0100

    LOGIST-209

---
+++ Chapter_02.py
@@ -0,0 +1,17 @@
+print("QUIZ TIME!")
+print ("Was ist 1+1?")
+a = 0
+b = 0
+a = int(input("Bitte Anwort eingeben: "))
+if a == 1:
+    print("Antwort ist richtig")
+else:
+    print("Antwort ist falsch")
+
+print("Wieviele Punkte hat das Telekom Logo")
+print("1. 1\n2. 2\n3. 3\n4. 4")
+b = int(input("Bitte Antwort eingeben: "))
+if b == 4:
+    print("Antwort ist richtig")
+else:
+    print("Antwort ist falsch")
