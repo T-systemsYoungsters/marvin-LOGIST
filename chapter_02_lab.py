91b117aea6081bbd1ad8aea739c10c66b4709416
MarvinKhn <92447962+MarvinKhn@users.noreply.github.com>
2021-11-19 00:46:12 +0100

    LOGIST-209

--- chapter_02_lab.py
+++ chapter_02_lab.py
@@ -1,13 +1,15 @@
 print("QUIZ TIME!")
-print ("Was ist 1+1?")
-true = ("Antwort ist richtig")
-false = ("Antwort ist falsch")
+true = ("\033[32m" + "Deine Antwort ist richtig!")
+false = ("\033[31m" + "Deine Antwort ist leider falsch.")
 a = 0
 b = 0
 c = 0
 d = 0
 e = 0
 count = 0
+
+print ("Was ist 1+1?")
+print()
 a = int(input("Bitte Anwort eingeben: "))
 if a == 2:
     print(true)
@@ -15,32 +17,34 @@ if a == 2:
 else:
     print(false)
 
-print()
+print("\033[0m")
 
-print("Wieviele Punkte hat das Telekom Logo")
-print("1. 1\n2. 2\n3. 3\n4. 4")
+print("Wie heißt der Vorstandsvositzende der Deustchen Telekom?")
+print("1. Timotheus Höttges\n2. Adel Al-Saleh\n3. Srini Gopalan\n4. Thorsten Langheim")
+print()
 b = int(input("Bitte Antwort eingeben: "))
-if b == 4:
+if b == 1 :
     print(true)
     count = count + 1
 else:
     print(false)
 
-print()
+print("\033[0m")
 
-print("Wieviele Ecken hat ein Oktaeder")
-print("1. 3\n2. 6\n3. 8\n4. 4")
+print("Wieviele Ecken hat ein Oktaeder?")
+print()
 c = int(input("Bitte Antwort eingeben: "))
-if c == 3:
+if c == 8:
     print(true)
     count = count + 1
 else:
     print(false)
 
-print()
+print("\033[0m")
 
 print("Wie heißt die erste deutsche Bundeskanzlerin?")
 print("1. Angelo\n2. Frauke Petri\n3. Claudia Roth\n4. Angela Merkel")
+print()
 d = int(input("Bitte Antwort eingeben: "))
 if d == 4:
     print(true)
@@ -48,9 +52,10 @@ if d == 4:
 else:
     print(false)
 
-print()
+print("\033[0m")
 
 print("Wieviel sind 50 Prozent von 100?")
+print()
 e = int(input("Bitte Antwort eingeben: "))
 if e == 50:
     print(true)
@@ -58,7 +63,9 @@ if e == 50:
 else:
     print(false)
 
-count_percent = (count /5) * 100 
+print("\033[0m")
 
-print("Herzlichen Glückwunsch, du hast", count,"Fragen richtig")
+count_percent = (count / 5) * 100 
+
+print("Herzlichen Glückwunsch, du hast", count,"Fragen richtig beantwortet!")
 print("Das entspricht", count_percent,"Prozent")
