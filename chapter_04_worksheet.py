b998b7b7f5418a089e40d17f5346f493080654f4
MarvinKhn <marvinkh13@gmail.com>
2022-01-07 13:13:48 +0100

    LOGIST-248

---
+++ chapter_04_worksheet.py
@@ -0,0 +1,18 @@
+'''
+#1
+for i in range (10):
+    print ("Marvin")
+print ("Done")
+
+#2
+for i in range (20):
+    print ("Red")
+    print ("Gold")
+'''
+#3
+c = 1
+for i in range (99):
+    c = c + 1
+    print (c)
+
+#4
