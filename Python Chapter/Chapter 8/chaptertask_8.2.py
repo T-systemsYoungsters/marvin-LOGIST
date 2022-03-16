8f918cebd2d758bace2838c9bfbc59fcd9434453
MarvinKhn <marvinkh13@gmail.com>
2022-03-16 02:32:13 +0100

    LOGIST-276

---
+++ Python Chapter/Chapter 8/chaptertask_8.2.py
@@ -0,0 +1,71 @@
+"""
+ Pygame base template for opening a window
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+ 
+import pygame
+import random
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+snow_list = []
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(BLACK)
+ 
+    # --- Drawing code should go here
+    for i in range(50):
+        x = random.randrange(0, 400)
+        y = random.randrange(0, 400)
+        snow_list.append([x, y])
+    for i in range(50):
+        x = random.randrange(0, 400)
+        y = random.randrange(0, 400)
+    # Process each snow flake in the list
+    for i in range(len(snow_list)):
+    # Draw the snow flake
+        pygame.draw.circle(screen, WHITE, snow_list[i], 2)
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+print(snow_list[0][1])
+# Close the window and quit.
+pygame.quit()
