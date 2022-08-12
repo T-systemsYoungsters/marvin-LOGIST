408a8437a9f6122ad6a3c63b9a7c37ac88dd6ad1
MarvinKhn <marvinkh13@gmail.com>
2022-08-12 15:00:30 +0200

    deliverynotebundle, loadcarrier, requests

--- DLS/python request/login.py
+++ DLS/python request/login.py
@@ -3,13 +3,17 @@ import json
 
 BASEURL = 'https://dlsbackend.test.cloud4log.dev/api/v2'
 
-auth = {
-  'emailAddress': 'marvin 8 1658927863978 u0@wa',
-  'password': 'Test12345678'
-  }
+def login ():
+  auth = {
+    'emailAddress': 'marvin 8 1658927863978 u0@wa',
+    'password': 'Test12345678'
+    }
 
-r = requests.post(BASEURL + '/authentication/login', json=auth)   
+  r = requests.post(BASEURL + '/authentication/login', json=auth)   
 
-print(r.url)
-print(r.text)
-print(r.status_code)
+  result = json.loads(r.text)
+
+  organizationSiteKey = result['organizations'][0]['sites'][0]['_key']
+  organizationKey = result['organizations'][0]['_key']
+
+  return {'sessionId':result['sessionId'],'organizationSiteKey':organizationSiteKey,'organizationKey':organizationKey}
