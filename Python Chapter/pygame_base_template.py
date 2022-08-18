899f43b7b733ae30b451262a7151b43482b3afab
MarvinKhn <marvinkh13@gmail.com>
2022-08-18 17:13:50 +0200

    pygame

--- Python Chapter/pygame_base_template.py
+++ Python Chapter/pygame_base_template.py
@@ -1,3 +1,14 @@
+"""
+ Pygame base template for opening a window
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+ 
 import pygame
  
 # Define some colors
@@ -5,8 +16,7 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-BROWN = (100, 40, 0)
-
+ 
 pygame.init()
  
 # Set the width and height of the screen [width, height]
@@ -14,23 +24,11 @@ size = (700, 500)
 screen = pygame.display.set_mode(size)
  
 pygame.display.set_caption("My Game")
-
+ 
+# Loop until the user clicks the close button.
 done = False
-
-def draw_stick_figure(screen, x, y):
-    # Head
-    pygame.draw.ellipse(screen, BLACK, [1+x,y,10,10], 0)
  
-    # Legs
-    pygame.draw.line(screen, BLACK ,[5+x,17+y], [10+x,27+y], 2)
-    pygame.draw.line(screen, BLACK, [5+x,17+y], [x,27+y], 2)
- 
-    # Body
-    pygame.draw.line(screen, RED, [5+x,17+y], [5+x,7+y], 2)
- 
-    # Arms
-    pygame.draw.line(screen, RED, [5+x,7+y], [9+x,17+y], 2)
-    pygame.draw.line(screen, RED, [5+x,7+y], [1+x,17+y], 2)
+# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
 # -------- Main Program Loop -----------
@@ -39,21 +37,20 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
-    pos = pygame.mouse.get_pos()
-    x = pos[0]
-    y = pos[1]
-
-    # Hide the mouse cursor
-    pygame.mouse.set_visible(False)
  
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
     # background image.
     screen.fill(WHITE)
  
     # --- Drawing code should go here
-    # Snowman in upper left
-    draw_stick_figure(screen,x,y)
-    
+ 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
